becb79139cc3a6c90d44c4ab9d42492169242c5b
Brahim Berkasse <berkassebrahim@gmail.com>
2024-01-15 20:30:09 +0100

    create TCP server and listen for incoming connection !

--- src/main.go
+++ src/main.go
@@ -1,9 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+const (
+	PORT        = ":6379"
+	BUFFER_SIZE = 1024
+	DELIMITER
+)
 
 func main() {
 
-	fmt.Println("Hello World !")
+	fmt.Println("Start listening for connection on 127.0.0.1" + PORT)
+
+	// create new server !
+	l, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatal("Can't run redos server :", err)
+	}
+
+	// listen for connection
+	conn, err := l.Accept()
+	if err != nil {
+		log.Fatal("Can't Accept connection : ", err)
+	}
+
+	defer conn.Close()
+
+	for {
+		buff := make([]byte, BUFFER_SIZE)
+		_, err := conn.Read(buff)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal("Something went wrong reading client query : ", err)
+		}
+
+		conn.Write([]byte("+OK\r\n"))
+	}
 
 }
