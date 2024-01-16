7c10e67b9622a3d8c234dd043818737c7a096f01
Brahim Berkasse <berkassebrahim@gmail.com>
2024-01-16 21:26:14 +0100

    handlers : adding get / set with mutexes locking the global set hashmap

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -1,11 +1,53 @@
 package handler
 
-import "github.com/brkss/redis/src/resp"
+import (
+	"sync"
+
+	"github.com/brkss/redis/src/resp"
+)
+
+var SETs = map[string]string{}
+var SETsMutex = sync.RWMutex{}
 
 var Handlers = map[string]func([]resp.Value) resp.Value{
 	"PING": ping,
+	"GET":  get,
+	"SET":  set,
 }
 
 func ping(args []resp.Value) resp.Value {
 	return resp.Value{Typ: "string", Str: "PONG"}
+}
+
+func set(args []resp.Value) resp.Value {
+	if len(args) != 2 {
+		return resp.Value{Typ: "error", Str: "ERR wrong number of argements for 'set' command"}
+	}
+
+	key := args[0].Blk
+	val := args[1].Blk
+
+	SETsMutex.Lock()
+	SETs[key] = val
+	SETsMutex.Unlock()
+
+	return resp.Value{Typ: "string", Str: "OK"}
+}
+
+func get(args []resp.Value) resp.Value {
+	if len(args) != 1 {
+		return resp.Value{Typ: "error", Str: "ERR wrong number of arguments for 'get' command"}
+	}
+
+	key := args[0].Blk
+
+	SETsMutex.RLock()
+	val, ok := SETs[key]
+	SETsMutex.RUnlock()
+
+	if !ok {
+		return resp.Value{Typ: "null"}
+	}
+
+	return resp.Value{Typ: "bulk", Blk: val}
 }
